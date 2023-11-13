6d01caaf158c4973154fd6bf49511d96e22bd677
abdul-aziz <abdulaziz.eu1@gmail.com>
2023-11-14 00:01:04 +0600

    Add Application DbContext File and Connect to the Database

---
+++ PointOfSaleWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PointOfSaleWeb.Models;
+
+namespace PointOfSaleWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+
+        }
+
+        public DbSet <Category> Categories {  get; set; }
+    }
+}
---
+++ PointOfSaleWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PointOfSaleWeb.Models
+{
+    public class Category
+    {
+      
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
