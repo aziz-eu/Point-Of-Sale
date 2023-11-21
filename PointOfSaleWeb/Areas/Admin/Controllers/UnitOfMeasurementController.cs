81705f2f586e335f7bd455d7d830deb0a5f0a639
abdul-aziz <abdulaziz.eu1@gmail.com>
2023-11-22 05:09:25 +0600

    CURD for Units of Measurement

---
+++ PointOfSaleWeb/Areas/Admin/Controllers/UnitOfMeasurementController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PointOfSale.Data;
+using PointOfSale.DataAccess.Repository.IRepository;
+using PointOfSale.Models;
+
+namespace PointOfSaleWeb.Areas.Admin.Controllers
+{
+    [Area ("Admin")]
+    public class UnitOfMeasurementController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public UnitOfMeasurementController (IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id) {
+
+            UnitsOfMeasurement unitsOfMeasurement = new();
+
+            if(id == 0 || id ==null)
+            {
+                // Create Category
+                return View(unitsOfMeasurement);
+            }
+            else
+            {
+                // Edit 
+                unitsOfMeasurement = _unitOfWork.UnitOfMeasurment.GetFirstOrDefault(x => x.Id == id);
+                return View(unitsOfMeasurement);
+            }
+        
+        
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(UnitsOfMeasurement unitsOfMeasurement)
+        {
+
+           if(ModelState.IsValid)
+            {
+                if (unitsOfMeasurement.Id == 0)
+                {
+                    _unitOfWork.UnitOfMeasurment.Add(unitsOfMeasurement);
+                    TempData["success"] = "New Unit Add Sccessful";
+                }
+                else
+                {
+                    _unitOfWork.UnitOfMeasurment.Update(unitsOfMeasurement);
+                    TempData["success"] = "Unit Update Successful";
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+
+           return View(unitsOfMeasurement);
+
+
+        }
+
+
+
+        #region API CALL
+
+        public IActionResult GetAll() {
+            var units = _unitOfWork.UnitOfMeasurment.GetAll();
+
+            return Json(new { data = units });
+        
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int? id) {
+
+            var units = _unitOfWork.UnitOfMeasurment.GetFirstOrDefault(u => u.Id == id);
+            if(units == null)
+            {
+                return Json(new {success =  false , message= "Error while deleteing" });
+            }
+            
+            _unitOfWork.UnitOfMeasurment.Remove(units);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+            
+        
+        
+        }
+
+        #endregion
+
+
+    }
+}
