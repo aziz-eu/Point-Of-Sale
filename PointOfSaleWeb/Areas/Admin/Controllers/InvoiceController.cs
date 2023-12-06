b46afac116ea0a122f3d31aa304fc6402c2b617a
abdul-aziz <abdulaziz.eu1@gmail.com>
2023-12-06 21:39:26 +0600

    fontend start from here

--- PointOfSaleWeb/Areas/Admin/Controllers/InvoiceController.cs
+++ PointOfSaleWeb/Areas/Admin/Controllers/InvoiceController.cs
@@ -27,8 +27,10 @@ namespace PointOfSaleWeb.Areas.Admin.Controllers
 
         public IActionResult Details(int id)
         {
+            var ammountInWord = NumberToWords(20020);
             InvoiceVM = new InvoiceVM()
             {
+                
                 InvoiceHeader = _unitOfWork.InvoiceHeader.GetFirstOrDefault(x => x.Id == id),
                 InvoiceDetails = _unitOfWork.InvoiceDetail.GetAll(x => x.InvoiceId == id, includeProperties : "Product")
 
@@ -38,7 +40,7 @@ namespace PointOfSaleWeb.Areas.Admin.Controllers
 
         public IActionResult Create()
         {
-
+            
             InvoiceVM invoiceVM = new InvoiceVM()
             {
                 Cart = new(),
@@ -262,5 +264,54 @@ namespace PointOfSaleWeb.Areas.Admin.Controllers
         #endregion
 
 
+        public static string NumberToWords(int number)
+        {
+            if (number == 0)
+                return "zero";
+
+            if (number < 0)
+                return "minus " + NumberToWords(Math.Abs(number));
+
+            string words = "";
+
+            if ((number / 1000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (number < 20)
+                    words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + unitsMap[number % 10];
+                }
+            }
+
+            return words;
+        }
     }
+
 }
